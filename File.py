21b3482849a86dd5d435421bfa2b01c96af981e0
ZainAbdeen <87270080+ZainAbdeen@users.noreply.github.com>
2023-06-10 21:50:40 +0500

    This Kaggle dataset, obtained from the National Institute of Diabetes and Digestive and Kidney Diseases, is a valuable resource for our project. The inclusion of parameters such as Glucose, Blood Pressure, Skin Thickness, Insulin, BMI, Diabetes Pedigree Function, and Age provides a comprehensive set of features that are known to be associated with diabetes.

---
+++ File.py
@@ -0,0 +1,51 @@
+import pandas as pd
+import numpy as np
+from sklearn.model_selection import train_test_split
+from sklearn.preprocessing import StandardScaler
+from sklearn.neighbors import KNeighborsClassifier
+from sklearn.metrics import confusion_matrix
+from sklearn.metrics import f1_score
+from sklearn.metrics import accuracy_score
+
+dataset = pd.read_csv('F:\DataSet\diabetes.csv')
+print(len(dataset))
+
+
+print(dataset.head())
+
+
+# Replace zeroes
+zero_not_accepted = ['Glucose', 'BloodPressure', 'SkinThickness', 'BMI', 'Insulin']
+
+for column in zero_not_accepted:
+    dataset[column] = dataset[column].replace(0, np.NaN)
+    mean = int(dataset[column].mean(skipna=True))
+    dataset[column] = dataset[column].replace(np.NaN, mean)
+
+# split dataset
+X = dataset.iloc[:, 0:8]
+y = dataset.iloc[:, 8]
+X_train, X_test, y_train, y_test = train_test_split(X, y, random_state=0, test_size=0.2)
+
+print(len(X_train))
+print(len(y_train))
+print(len(X_test))
+print(len(y_test))
+
+sc_X = StandardScaler()
+X_train = sc_X.fit_transform(X_train)
+X_test = sc_X.transform(X_test)
+
+classifier = KNeighborsClassifier(n_neighbors=11, p=2,metric='euclidean')
+
+classifier.fit(X_train, y_train)
+
+y_pred = classifier.predict(X_test)
+y_pred
+
+cm = confusion_matrix(y_test, y_pred)
+print (cm)
+print(f1_score(y_test, y_pred))
+
+print(accuracy_score(y_test, y_pred))
+
